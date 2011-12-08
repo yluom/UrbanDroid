4ab2a66e0cdd077d240982c734d4a11e7a7a379f
Flop <flop@kakou-corp.fr>
2011-12-08 22:44:56 +0100

    update avant dodo

---
+++ src/fr/android/urbandroid/MiseAJour.java
@@ -0,0 +1,89 @@
+package fr.android.urbandroid;
+
+import java.net.*;
+import java.io.*;
+
+/*
+* Classe MiseAJour
+* 
+* Derniere modification : Flo
+* 
+* Utilisation :
+* 
+* MiseAJour maj("http://www.site.com/version.txt", "1");
+* if (maj.nouvelle())
+* 	maj.faire();
+* 
+*/
+
+public class MiseAJour
+{
+	
+	// l'url que l'on recupere en parametre
+	private String url;
+	private String versionActuelle;
+	
+	// Constructeur
+	public MiseAJour(String urlParam, String version) throws Exception
+	{
+		this.url = urlParam;
+		if (!this.testURL())
+			throw new Exception("URL de mise a jour invalide");
+	}
+	
+	private String getVersion()
+	{
+		try
+		{
+			URL checkVersion = new URL(this.url);
+			URLConnection co = checkVersion.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(co.getInputStream()));
+			String version = in.readLine();
+			in.close();
+			return version;
+		}
+		catch (Exception ex)
+		{
+			// aficher un message d'erreur
+			System.out.println(ex.toString());
+		}
+		return null;
+	}
+	
+	// Verifier que l'url est valide
+	private boolean testURL()
+	{
+		if (this.getVersion() == null || this.getVersion() == "")
+			return false;
+		else
+			return true;
+	}
+
+	public boolean nouvelle() throws Exception
+	{
+		if (!this.testURL())
+			throw new Exception("URL de mise a jour invalide");
+		if (Integer.parseInt(this.versionActuelle) < Integer.parseInt(this.getVersion()))
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean faire()
+	{
+		try
+		{
+			if (!this.nouvelle())
+				return false;
+			File f = new File("urbandroid.bdd");
+			if (f.exists())
+				f.delete();
+			// downloader le fichier
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex.toString());
+		}
+		
+	}
+}
