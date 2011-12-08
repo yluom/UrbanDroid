a7e65d964f5227fa7f6c8bfd2cd74ccee06652f8
Léo <leo.mouly@gmail.com>
2011-12-08 20:13:57 +0100

    Ajout du code de base d'urb

---
+++ src/urban/droid/UrbandroidActivity.java
@@ -0,0 +1,13 @@
+package urban.droid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UrbandroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
