1f70ecf52426e634b1a80345e678509f21531853
Léo <leo.mouly@gmail.com>
2011-12-14 07:49:09 +0100

    ajout des xml <3

--- gen/fr/android/urbandroid/R.java
+++ gen/fr/android/urbandroid/R.java
@@ -11,13 +11,33 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int boutonitinoff=0x7f020000;
+        public static final int boutonplon=0x7f020001;
+        public static final int boutontarifoff=0x7f020002;
+        public static final int favoff=0x7f020003;
+        public static final int horroff=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int planoff=0x7f020007;
+    }
+    public static final class id {
+        public static final int fav=0x7f050004;
+        public static final int hor=0x7f050003;
+        public static final int itin=0x7f050001;
+        public static final int plan=0x7f050000;
+        public static final int tarif=0x7f050002;
+        public static final int textView1=0x7f050005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int favoris=0x7f030000;
+        public static final int horaires=0x7f030001;
+        public static final int itineraire=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int plan=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int plan=0x7f040002;
     }
 }
