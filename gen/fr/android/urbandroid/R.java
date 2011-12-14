9e834d76a2f05c5bf05bb04168460b0e8521176c
Léo <leo.mouly@gmail.com>
2011-12-14 15:10:02 +0100

    squelette de l'interface xml ! ajout des many .jpeg

--- gen/fr/android/urbandroid/R.java
+++ gen/fr/android/urbandroid/R.java
@@ -11,33 +11,48 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int boutonitinoff=0x7f020000;
-        public static final int boutonplon=0x7f020001;
-        public static final int boutontarifoff=0x7f020002;
-        public static final int favoff=0x7f020003;
-        public static final int horroff=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int icon=0x7f020006;
-        public static final int planoff=0x7f020007;
+        public static final int band=0x7f020000;
+        public static final int bandtar=0x7f020001;
+        public static final int boutonitinoff=0x7f020002;
+        public static final int boutonplon=0x7f020003;
+        public static final int boutontarifoff=0x7f020004;
+        public static final int favb=0x7f020005;
+        public static final int favg=0x7f020006;
+        public static final int favoff=0x7f020007;
+        public static final int gmap=0x7f020008;
+        public static final int horb=0x7f020009;
+        public static final int horg=0x7f02000a;
+        public static final int horroff=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int icon=0x7f02000d;
+        public static final int itib=0x7f02000e;
+        public static final int itig=0x7f02000f;
+        public static final int onggmapb=0x7f020010;
+        public static final int onglettisseog=0x7f020011;
+        public static final int plab=0x7f020012;
+        public static final int plag=0x7f020013;
+        public static final int planoff=0x7f020014;
+        public static final int tarb=0x7f020015;
+        public static final int targ=0x7f020016;
+        public static final int tisseo=0x7f020017;
     }
     public static final class id {
-        public static final int fav=0x7f050004;
-        public static final int hor=0x7f050003;
-        public static final int itin=0x7f050001;
-        public static final int plan=0x7f050000;
-        public static final int tarif=0x7f050002;
-        public static final int textView1=0x7f050005;
+        public static final int btn_fav=0x7f050001;
+        public static final int btn_hor=0x7f050002;
+        public static final int btn_iti=0x7f050004;
+        public static final int btn_pla=0x7f050003;
+        public static final int btn_tar=0x7f050005;
+        public static final int imageView6=0x7f050006;
+        public static final int imageView8=0x7f050008;
+        public static final int imageView9=0x7f050007;
+        public static final int titre=0x7f050000;
     }
     public static final class layout {
-        public static final int favoris=0x7f030000;
-        public static final int horaires=0x7f030001;
-        public static final int itineraire=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int plan=0x7f030004;
+        public static final int plan=0x7f030000;
+        public static final int tarif=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
-        public static final int plan=0x7f040002;
     }
 }
