f1ac8835260f06409625531ecca13ddef70fb6f9
Flop <flop@kakou-corp.fr>
2011-12-22 12:41:51 +0100

    Coeur du système de mise à jour terminé.
    TODO :
    - Faire la premiere activité avec un logo de chargement pendant la mise à jour
    - Puis une fois la mise à jour vérifié et (peut-être) la nouvelle bdd téléchargé, passer au l'activité principale (ici celle de la layout plant)

--- src/fr/android/urbandroid/UrbanDroidActivity.java
+++ src/fr/android/urbandroid/UrbanDroidActivity.java
@@ -1,60 +1,79 @@
 package fr.android.urbandroid;
 
-import fr.android.urbandroid.*;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.*;
+import java.net.*;
+
 
 public class UrbanDroidActivity extends Activity {
+	
+	private static final String TAG = "UrbanDroidActivity";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plant);
-        /* CODE DE LA MISE A JOUR !!! 
-	        * 
-	        * TODO= regler la levÃ©e d'exception "unknowhostexception" de nixitup
-	        * 
-	          File f = new File("/data/data/fr.android.urbandroid/files/urbdroid.bdd");
-	        
-	        if (!f.exists())
-	        {
-	        	Resources res = getResources();
-	        	String urlBdd = res.getString(R.string.urlBdd);
-	        	
-	        	 URL url = new URL(urlBdd);
-	             URLConnection conexion = url.openConnection();
-	             conexion.connect();
-	             // this will be useful so that you can show a tipical 0-100% progress bar
-	             int lenghtOfFile = conexion.getContentLength();
-plant
-	             // download the file
-	             InputStream input = new BufferedInputStream(url.openStream());
-	        	 f.createNewFile();
-	        	 BufferedOutputStream bis = new BufferedOutputStream(new FileOutputStream(f));
-	             int count;
-	             byte data[] = new byte[1024];
-	             while ((count = input.read(data)) != -1) 
-	             {
-	            	 bis.write(data);
-	             }
-
-     		bis.flush();
-     		bis.close();
-     		input.close();
-     		
-	        	//afficher pas ok
-	        }
-	        else
-	        {
-	        	//afficher contenu
-	        	//Toast.makeText(this, "Djir, le fichier existe!", 10).show();
-	        	
-	        }*/
+        
+        /*
+         * CODE DE MISE A JOUR
+         */
+		// Nos "pointeurs" de fichier
+		File ptrBdd = new File("/data/data/fr.android.urbandroid/files/urbdroid.bdd");
+		File ptrVersion = new File("/data/data/fr.android.urbandroid/files/bdd.version");
+		
+		// Nos ressources utiles
+		Resources res = getResources();
+		String urlBdd = res.getString(R.string.urlBdd);
+		String urlVersionBdd = res.getString(R.string.urlVersionBdd);
+		try
+		{
+			// Cas ou il n'y a pas de bdd : premiere utilisation.
+			// On download la bdd et le numéro de version
+			if (!ptrBdd.exists())
+			{	
+				ptrBdd.createNewFile();
+				ptrVersion.createNewFile();
+				this.download(urlBdd, ptrBdd);
+				this.download(urlVersionBdd, ptrVersion);
+				Toast.makeText(this, "New BDD downloaded! :)\nBDD Version = " + this.fileContents(ptrVersion), 10).show();
+			}
+			
+			// Cas ou on a deja la bdd : N utilisations.
+			// On download la nouvelle bdd que si il y a besoin
+			else
+			{
+				// Comparer les versions
+				int actualVersion = Integer.parseInt(this.fileContents(ptrVersion));
+				int checkVersion = Integer.parseInt(this.webContents(urlVersionBdd));
+				if (actualVersion < checkVersion)
+				{
+					this.download(urlBdd, ptrBdd);
+					this.download(urlVersionBdd, ptrVersion);
+					Toast.makeText(this, "New BDD downloaded! :)\nBDD Version = " + this.fileContents(ptrVersion), 10).show();
+				}
+				else
+					Toast.makeText(this, "BDD already up to date! :)\nBDD Version = " + this.fileContents(ptrVersion), 10).show();	
+			}
+		}
+		catch (Exception ex)
+		{
+			Toast.makeText(this, "1 :" + ex.toString(), 10).show();
+			Log.e(TAG, Log.getStackTraceString(ex));
+		}
+		/*
+		 * FIN DE LA MISE A JOUR
+		 */
         
         // code qui gere les transistions du menu
 
@@ -99,6 +118,83 @@ plant
         
         ImageView iv8 = (ImageView) findViewById(R.id.plantisseo);
         iv8.setOnTouchListener(new Touch());
+    }
+    
+    public void download(String link, File f)
+    {
+    	try
+    	{
+    		if (f.exists())
+    			f.delete();
+    		f.createNewFile();
+    		Toast.makeText(this, "Link = " + link + "\nFile = " + f.getPath() + " @ " + f.getName(), 10).show();
+	    	URL url = new URL(link);
+			URLConnection conexion = url.openConnection();
+			conexion.connect();
+			
+			// téléchargement du fichier
+			InputStream input = new BufferedInputStream(url.openStream());
+			OutputStream os = new FileOutputStream(f.getPath());
+			int count;
+			byte data[] = new byte[1024];
+			while ((count = input.read(data)) != -1) 
+			{
+				os.write(data, 0, count);
+			}
+			
+			os.flush();
+			os.close();
+			input.close();
+		
+    	}
+    	catch (Exception ex)
+    	{
+    		Toast.makeText(this, "2 :" + ex.toString(), 10).show();
+    		Log.e(TAG, Log.getStackTraceString(ex));
+    	}
+    }
+    
+    public String fileContents(File f)
+    {
+    	try
+    	{
+            FileInputStream fStream = new FileInputStream(f);
+            String res = "";
+            BufferedReader in = new BufferedReader(new InputStreamReader(fStream));
+            while (in.ready())
+                res += in.readLine();
+            in.close();
+			return res.toString();
+    	}
+    	catch (Exception ex)
+    	{
+    		Toast.makeText(this, "3 :" + ex.toString(), 10).show();
+    		Log.e(TAG, Log.getStackTraceString(ex));
+    	}
+		return null;
+    }
+    public String webContents(String link)
+    {
+    	try
+    	{
+	    	URL url = new URL(link);
+			URLConnection conexion = url.openConnection();
+			conexion.connect();
+			InputStream input = new BufferedInputStream(url.openStream());
+            String res = "";
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            while (in.ready())
+                res += in.readLine();
+            in.close();
+            input.close();
+			return res.toString();
+    	}
+    	catch (Exception ex)
+    	{
+    		Log.e(TAG, Log.getStackTraceString(ex));
+    		Toast.makeText(this, "4 :" + ex.toString(), 10).show();
+    	}
+    	return null;
     }
 
 }
