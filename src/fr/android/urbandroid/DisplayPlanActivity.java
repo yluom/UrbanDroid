b5ba38a1895df1290d9157429bb3aae8bd69292a
Flop <flop@kakou-corp.fr>
2012-01-05 14:06:36 +0100

    Plan tisseo = almost done.

--- src/fr/android/urbandroid/DisplayPlanActivity.java
+++ src/fr/android/urbandroid/DisplayPlanActivity.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.util.FloatMath;
+import android.util.Log;
+import android.view.MotionEvent;
 
-public class DisplayPlanActivity extends Activity {
+public class DisplayPlanActivity extends Activity implements OnTouchListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,6 @@ public class DisplayPlanActivity extends Activity {
         setContentView(R.layout.plant);
         
         // code qui gere les transistions du menu
-
         OnClickListener menuSwitcher = new OnClickListener()
         {
           public void onClick(View actualView)
@@ -58,7 +63,81 @@ public class DisplayPlanActivity extends Activity {
         iv7.setOnClickListener(menuSwitcher);
         
         ImageView iv8 = (ImageView) findViewById(R.id.plantisseo);
-        iv8.setOnTouchListener(new Touch());
+        iv8.setOnTouchListener(this);
     }
-
+    
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist;
+    private static final String TAG = "Touch" ;
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+    
+    private float spacing(MotionEvent event) 
+    {
+    	float x = event.getX(0) - event.getX(1);
+    	float y = event.getY(0) - event.getY(1);
+    	return FloatMath.sqrt(x * x + y * y);
+	}
+    
+    private void midPoint(PointF point, MotionEvent event) {
+    	   float x = event.getX(0) + event.getX(1);
+    	   float y = event.getY(0) + event.getY(1);
+    	   point.set(x / 2, y / 2);
+    	}
+    
+	public boolean onTouch(View v, MotionEvent event) 
+	{
+		ImageView view = (ImageView) v;
+		switch (event.getAction() & MotionEvent.ACTION_MASK) 
+		{
+			case MotionEvent.ACTION_DOWN:
+				savedMatrix.set(matrix);
+				start.set(event.getX(), event.getY());
+				Log.d(TAG, "mode=DRAG" );
+				mode = DRAG;
+			    break;
+			case MotionEvent.ACTION_UP:
+			case MotionEvent.ACTION_POINTER_UP:
+				mode = NONE;
+				Log.d(TAG, "mode=NONE" );
+				break;
+			case MotionEvent.ACTION_POINTER_DOWN:
+				oldDist = spacing(event);
+				Log.d(TAG, "oldDist=" + oldDist);
+				if (oldDist > 10f) 
+				{
+					savedMatrix.set(matrix);
+					midPoint(mid, event);
+					mode = ZOOM;
+					Log.d(TAG, "mode=ZOOM" );
+				}
+				break;
+				
+		   case MotionEvent.ACTION_MOVE:
+			   if (mode == DRAG) 
+			   {
+				   matrix.set(savedMatrix);
+				   matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+			   }
+			   else if (mode == ZOOM) 
+			   {
+		    	  float newDist = spacing(event);
+		    	  Log.d(TAG, "newDist=" + newDist);
+		    	  if (newDist > 10f) 
+		    	  {
+		    		  matrix.set(savedMatrix);
+		    		  float scale = newDist / oldDist;
+		    		  matrix.postScale(scale, scale, mid.x, mid.y);
+		    	  }
+		    	  break;
+			   }	
+		}
+		view.setImageMatrix(matrix);
+		return true;
+	}
 }
